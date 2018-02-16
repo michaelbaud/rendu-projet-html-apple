a092b338ee579d7f9acb083eaa42e55cbf5e75e0
michaelbaud <michael.baudoin.7@gmail.com>
2018-02-16 17:34:35 +0100

    michael apple projet

---
+++ main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+
+});
+
+// menu responsive
+var btn = document.querySelector(".logoMenu");
+var nav = document.querySelector(".nav");
+
+btn.onclick = function() {
+	nav.classList.toggle("nav_open");
+}
